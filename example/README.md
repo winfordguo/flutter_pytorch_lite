744612e97a189f89025e78c33bc6c5ea09b18c06
winfordguo <winfordguo@gmail.com>
2024-01-15 11:21:45 +0800

    example

--- example/README.md
+++ example/README.md
@@ -1,16 +1,5 @@
 # flutter_pytorch_lite_example
 
-Demonstrates how to use the flutter_pytorch_lite plugin.
+A simple image classification application that demonstrates how to use the `flutter_pytorch_lite` plugin.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+<p><img src="screenshots/1.jpg" alt="Flutter PyTorch Lite Example" /></p>
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_pytorch_lite/flutter_pytorch_lite.dart';
 
@@ -16,26 +19,40 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  static const assetImage = AssetImage('assets/images/image.png');
+
+  ImageClassificationHelper helper = ImageClassificationHelper();
+  Map<String, double>? classification;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    helper.initHelper();
+
+    classified();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      await FlutterPytorchLite.destroy();
-    } on PlatformException {}
+  Future<void> classified() async {
+    ui.Image image = await _loadImage();
+    classification = await helper.inferenceImage(image);
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
+    setState(() {});
+  }
 
+  Future<ui.Image> _loadImage() {
+    Completer<ui.Image> completer = Completer.sync();
+    assetImage.resolve(ImageConfiguration.empty).addListener(
+        ImageStreamListener((ImageInfo image, bool synchronousCall) {
+      if (!completer.isCompleted) completer.complete(image.image);
+    }));
+    return completer.future;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    helper.close();
   }
 
   @override
@@ -43,12 +60,121 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Flutter PyTorch Lite'),
         ),
-        body: const Center(
-          child: Text('Hello, Flutter PyTorch Lite!\n'),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text('A simple image classification application!\n'),
+              const Image(image: assetImage),
+              if (classification != null)
+                Expanded(
+                    child: SingleChildScrollView(
+                  child: Text(
+                    classification!.keys
+                        .map((key) => '$key: ${classification?[key]}')
+                        .join('\n'),
+                    textAlign: TextAlign.center,
+                  ),
+                )),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class ImageClassificationHelper {
+  static const modelPath = 'assets/models/model.ptl';
+  static const labelsPath = 'assets/models/words.txt';
+
+  late final List<String> labels;
+  final Int64List inputShape = Int64List.fromList([1, 3, 224, 224]);
+  final Int64List outputShape = Int64List.fromList([1, 1000]);
+
+  // Load model
+  Future<void> _loadModel() async {
+    final filePath = '${Directory.systemTemp.path}/model.ptl';
+    File(filePath).writeAsBytesSync(await _getBuffer(modelPath));
+    await FlutterPytorchLite.load(filePath);
+
+    print('Interpreter loaded successfully');
+  }
+
+  /// Get byte buffer
+  static Future<Uint8List> _getBuffer(String assetFileName) async {
+    ByteData rawAssetFile = await rootBundle.load(assetFileName);
+    final rawBytes = rawAssetFile.buffer.asUint8List();
+    return rawBytes;
+  }
+
+  // Load labels from assets
+  Future<void> _loadLabels() async {
+    final labelTxt = await rootBundle.loadString(labelsPath);
+    labels = labelTxt.split('\n');
+  }
+
+  Future<void> initHelper() async {
+    _loadLabels();
+    _loadModel();
+  }
+
+  // inference still image
+  Future<Map<String, double>> inferenceImage(ui.Image image) async {
+    final height = inputShape[2];
+    final width = inputShape[3];
+    ui.Image imageInput = await _resizeImage(image, width, height);
+
+    // rgba
+    final pixels = (await imageInput.toByteData(
+            format: ui.ImageByteFormat.rawExtendedRgba128))!
+        .buffer
+        .asFloat32List();
+    // rgb
+    final imageMatrix = Float32List.fromList(
+        List.generate(inputShape[0] * inputShape[1] * height * width, (index) {
+      final pixelIdx = index ~/ inputShape[1];
+      final rgbIdx = index % inputShape[1];
+      return pixels[pixelIdx * 4 + rgbIdx];
+    }));
+    Tensor inputTensor = Tensor.fromBlobFloat32(imageMatrix, inputShape);
+
+    // Forward
+    Tensor outputTensor = await FlutterPytorchLite.forward(inputTensor);
+
+    // Get output tensor
+    final result = outputTensor.dataAsFloat32List;
+
+    // Set classification map {label: points}
+    var classification = <String, double>{};
+    for (var i = 0; i < result.length; i++) {
+      if (result[i] != 0) {
+        // Set label: points
+        classification[labels[i]] = result[i];
+      }
+    }
+    return classification;
+  }
+
+  // Resizes an [ui.Image] to a given [targetWidth] and [targetHeight]
+  Future<ui.Image> _resizeImage(
+      ui.Image image, int targetWidth, int targetHeight) {
+    final recorder = ui.PictureRecorder();
+    final canvas = ui.Canvas(recorder);
+
+    canvas.drawImageRect(
+      image,
+      ui.Rect.fromLTRB(0, 0, image.width.toDouble(), image.height.toDouble()),
+      ui.Rect.fromLTRB(0, 0, targetWidth.toDouble(), targetHeight.toDouble()),
+      ui.Paint(),
+    );
+
+    return recorder.endRecording().toImage(targetWidth, targetHeight);
+  }
+
+  Future<void> close() async {
+    await FlutterPytorchLite.destroy();
   }
 }
